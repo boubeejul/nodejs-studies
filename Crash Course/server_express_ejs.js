1a092070a8151dbe6200779563c1fd0591eaa51b
boubeejul <juliana.boubee@gmail.com>
2023-10-20 16:13:25 -0300

    crash course lesson 8

--- Crash Course/server_express_ejs.js
+++ Crash Course/server_express_ejs.js
@@ -7,19 +7,25 @@ server.listen(3333);
 server.set('view engine', 'ejs');
 server.set('views', 'views_ejs');
 
+// middleware & static files
+// -> make files public to access
+// -> 'public' == folder's name
+server.use(express.static('public'));
+
 server.get('/', (req, res) => {
   const blogs = [
     {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
     {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
     {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
   ];
+
+  
   // the first parameter is the file's name to be rendered without the extension
   // the second parameter sends data to the EJS file
 
   // insert javascript in EJS
   // <% line of code here %>
   // if it is a single value === <%= value here %>
-
   res.render('index', {
     title: 'Home',
     blogs: blogs
